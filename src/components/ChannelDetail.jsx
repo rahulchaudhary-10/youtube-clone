3e54793be8da059719e5f42d1cec0ab60f023999
Rahul Chaudhary <110348098+rahulchaudhary-10@users.noreply.github.com>
2023-04-20 21:51:09 +0530

    s

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -3,9 +3,18 @@ import {useParams} from 'react-router-dom';
 import {Box} from '@mui/material';
 
 import {Videos,ChannelCard} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 const ChannelDetail = () => {
+    const [ChannelDetail, setChannelDetail]=useState(null)
+    const {id} = useParams();
+    console.log(ChannelDetail);
+
+    useEffect(()=>{
+        fetchFromAPI(`channels?part="snippet&id"=${id}`)
+        .then((data)=> setChannelDetail(data?.items[0]));
+    },[id])
   return (
-    <div>ChannelDetail</div>
+    <div>{id}</div>
   )
 }
 
